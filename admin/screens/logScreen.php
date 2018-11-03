07fd7764dbe661274eb663fb45a999c534e5edf0
Kirian <kirian.caumes@ynov.com>
2018-11-03 15:55:09 +0100

    fix issue with confirm modal

--- admin/screens/logScreen.php
+++ admin/screens/logScreen.php
@@ -10,7 +10,9 @@ class LogScreen extends MasterScreen {
         return self::$_instance;
     }
 
-    private function __construct() {}	
+    private function __construct() {
+		wp_enqueue_script('commonJs', plugin_dir_url( __FILE__ ).'/../../js/common.js', '', '', true); 
+	}	
     
     function display(){
         ?>

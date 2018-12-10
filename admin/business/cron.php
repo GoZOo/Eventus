253e904dff3ce3367a1c0b3fb93428759263c7f4
Caumes Kirian <kirian.caumes@nextdecision.fr>
2018-12-10 18:43:12 +0100

    update cron2

--- admin/business/cron.php
+++ admin/business/cron.php
@@ -19,17 +19,19 @@ foreach (DAO\TeamDAO::getInstance()->getAllTeams() as $team) {
 }
 update_option('eventus_datetimesynch', date("Y-m-d H:i:s"), false);
 
-$message = "<p>The update has been succesfully done with: <b>". count(file('../../finder.log')) ."</b> issue(s), the <b>".date("d/m/Y")."</b> at <b>".date("H:i:s")."</b>.</p>";
-$content = explode("\n", file_get_contents('../../finder.log'));
-($content ? array_pop($content) : '' );
-$message .= ($content ? "<ul><li>".str_replace("[", "<b>[", str_replace("]", "]</b>", implode("</li><li>", $content)))."</ul>" : '');
+if (get_option("eventus_emailnotif")){
+	$message = "<p>The update has been succesfully done with: <b>". count(file('../../finder.log')) ."</b> issue(s), the <b>".date("d/m/Y")."</b> at <b>".date("H:i:s")."</b>.</p>";
+	$content = explode("\n", file_get_contents('../../finder.log'));
+	($content ? array_pop($content) : '' );
+	$message .= ($content ? "<ul><li>".str_replace("[", "<b>[", str_replace("]", "]</b>", implode("</li><li>", $content)))."</ul>" : '');
+	mail(
+		get_option("eventus_emailnotif"), 
+		"Eventus - Update ".date("d/m/Y H:i:s"), 
+		$message, 
+		"From: eventus@".$_SERVER['HTTP_HOST']. "\r\n" .
+		"Reply-To: eventus@".$_SERVER['HTTP_HOST'] ."\r\n" .
+		"X-Mailer: PHP/" . phpversion()
+	);
+}
 
-mail(
-	get_option("eventus_emailnotif"), 
-	"Eventus - Update ".date("d/m/Y H:i:s"), 
-	$message, 
-	"From: eventus@".$_SERVER['HTTP_HOST']. "\r\n" .
-    "Reply-To: eventus@".$_SERVER['HTTP_HOST'] ."\r\n" .
-    "X-Mailer: PHP/" . phpversion()
-);
 ?> 
